b0a5cc0ffa569fb8e25f1af0c2b444aca7325c18
MARÍA CARMONA PASTOR <Maria.Carmona1@alu.uclm.es>
2020-12-24 17:23:36 +0100

    Changing State constructor to match new Utils methods and deleting unused code that was giving errors after said change

--- Assignment2_IS/src/chess/State.java
+++ Assignment2_IS/src/chess/State.java
@@ -10,25 +10,27 @@ public class State {
 	public int m_boardSize = -1; 
 	
 	// constructor
-	public State(int[][] board, Position pos, int a){
+	public State(int[][] board){//, Position pos, int a){
 		m_board = board;
-		m_agentPos = pos;
-		m_agent = a;
+		//m_agentPos = pos;
+		//m_agent = a;
 		
-		if (m_agent >11){
+		/*if (m_agent >11){
 			System.out.println("\n*** Invalid piece ***\n");
 			System.exit(0);
 		}
 		else if (m_agent >5) m_color = 1; // black
-		
+		*/
 		m_boardSize = board[0].length;
 	}
 
 	// compares if the current state is final, i.e. the agent is in the last row
+//	public boolean isFinal(){
+//            return this.m_agentPos.row == this.m_boardSize-1;
+//	}
 	public boolean isFinal(){
-            return this.m_agentPos.row == this.m_boardSize-1;
-	}
-	
+            return true;
+        }
 	// hard copy of an State
 	public State copy(){
 		int[][] cBoard = new int[this.m_boardSize][this.m_boardSize];
@@ -36,7 +38,7 @@ public class State {
 		for(int r=0;r<this.m_boardSize;r++)
 			System.arraycopy(this.m_board[r], 0, cBoard[r], 0, this.m_boardSize);
 		
-		return new State(cBoard, this.m_agentPos.copy(),m_agent);
+		return new State(cBoard);//, this.m_agentPos.copy(),m_agent);
 	}
 	
 	// apply a given action over the current state -which remains unmodified. Return a new state
