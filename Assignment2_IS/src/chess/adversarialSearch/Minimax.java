8c60817c34c7a71da9ec9dbc8ef01d66025dc1cd
MARÍA CARMONA PASTOR <Maria.Carmona1@alu.uclm.es>
2020-12-29 22:09:17 +0100

    Sigue sin ir.

--- Assignment2_IS/src/chess/adversarialSearch/Minimax.java
+++ Assignment2_IS/src/chess/adversarialSearch/Minimax.java
@@ -3,6 +3,7 @@ package chess.adversarialSearch;
 import chess.Action;
 import chess.Position;
 import chess.State;
+import chess.Utils;
 import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Integer.MIN_VALUE;
 import java.util.ArrayList;
@@ -12,7 +13,8 @@ public class Minimax extends Adversarial {
 
     boolean player = true; //true = max, false = min
     ArrayList<Action> allMovements = new ArrayList<>();
-    ArrayList<Action> toChooseFrom = new ArrayList<>();
+    ArrayList<Action> toChooseFrom;
+    double[] valores;
 
     public Minimax(State s, int maxDepth, int maxTurns) {
         super(s, maxDepth, maxTurns);
@@ -34,7 +36,7 @@ public class Minimax extends Adversarial {
         //int i = 0, row = 0, column = 0;
         for (int r = 0; r < n; r++) {
             for (int c = 0; c < n; c++) {
-                if (state.m_color == color && state.m_board[r][c]!=12) {
+                if (state.m_board[r][c]!=12 && (state.m_color=Utils.getColorPiece(state.m_board[r][c])) == color) {
                     state.m_agent = state.m_board[r][c];
                     piece = choosePiece(state);
                     state.m_agentPos = new Position(r, c);
@@ -53,15 +55,14 @@ public class Minimax extends Adversarial {
         Action a = null;
         //depth = 0;
         movements(0);
-        
-        for(int i= 0; i<allMovements.size();i++){
-            toChooseFrom.set(i,allMovements.get(i));
-        }
+        valores = new double[allMovements.size()];
+        System.out.println("Numero de posibles acciones: "+ allMovements.size());
+        toChooseFrom = (ArrayList<Action>)allMovements.clone();
         double value = maxValue(s, 0);
 
-        for (int i = 0; i < allMovements.size(); i++) {
-            if (maxValue(s.applyAction(allMovements.get(i)), 1) == value) {
-                a = allMovements.get(i);
+        for (int i = 0; i < toChooseFrom.size(); i++) {
+            if (valores[i] == value) {
+                a = toChooseFrom.get(i);
                 break;
             }
         }
@@ -70,7 +71,7 @@ public class Minimax extends Adversarial {
     }
 
     public double maxValue(State s, int dmax) {
-
+        int proban2;
         double value;
         int dMax=dmax+1;
         if (s.isFinal()) {
@@ -80,20 +81,25 @@ public class Minimax extends Adversarial {
         value = MIN_VALUE;
         
         //depth=depth+1;
-        System.out.println("MAX After IsFinal: "+ dMax);
+        //System.out.println("MAX After IsFinal: "+ dMax);
         //System.out.println("Limit depht: "+ maxDepth);
         if (dMax >= maxDepth) {
             return utility(s);
         }
         
-        System.out.println("MAX After maxDepth: "+ dMax);
+        //System.out.println("MAX After maxDepth: "+ dMax);
+        ArrayList<Action> allMovements = new ArrayList<>();
         movements(0);
-        for (int i = 0; i < allMovements.size(); i++) {
+        proban2 = allMovements.size();
+        System.out.println("Proban2: "+ proban2);
+        for (int i = 0; i < proban2; i++) {
             value = Math.max(value, minValue(s.applyAction(allMovements.get(i)), dMax));
-            
+            if(dMax==1){
+                valores[i] = value;
+            }
         }
         
-        System.out.println("MAX After for: "+ dMax);
+        //System.out.println("MAX After for: "+ dMax);
         return value;
     }
 
@@ -108,19 +114,19 @@ public class Minimax extends Adversarial {
         value = MAX_VALUE;
 
         //depth=depth+1;
-        System.out.println("Min After IsFinal: "+ dMin);
+        //System.out.println("Min After IsFinal: "+ dMin);
         //System.out.println("Limit depht: "+ maxDepth);
         if (dMin >= maxDepth) {
             //System.out.println("Entra en if");
             return utility(s);
             
         }
-        System.out.println("MIN After maxDepth: "+ dMin);
+        //System.out.println("MIN After maxDepth: "+ dMin);
         movements(1);
         for (int i = 0; i < allMovements.size(); i++) {
             value = Math.min(value, maxValue(s.applyAction(allMovements.get(i)), dMin));
         }
-        System.out.println("MIN After for: "+ dMin);
+        //System.out.println("MIN After for: "+ dMin);
         return value;
 
     }
